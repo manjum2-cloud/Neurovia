dbada080d26386398f6a25facdc089adc626ea16
Mustaisam Anjum <manjum2@seu.edu.ge>
2026-03-29 18:02:03 +0400

    LoginButton.js

---
+++ pages/api/auth/components/LoginButton.js
@@ -0,0 +1,16 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function LoginButton() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <p>Signed in as {session.user.email}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+
+  return <button onClick={() => signIn("google")}>Sign in with Google</button>;
+}
